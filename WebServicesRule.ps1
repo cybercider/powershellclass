396aa5356320fbc5597d34929bf5188150121500
cybercider <wardo.exe@gmail.com>
2019-06-13 16:15:37 -0500

    Update WebServicesRule.ps1

--- WebServicesRule.ps1
+++ WebServicesRule.ps1
@@ -16,5 +16,23 @@ Clicking on the webservice module name gives you the URL which can be used
 out-gridview
 convertto-csv
 convertto-html
+Build own webservice Google MDT Web service sample
+Something like below...close at least :)
 #>
 
+$URI = "http://www.dneonline.com/calculator.asmx"
+$Web = New-WebServiceProxy -Uri $URI
+
+$web | Get-Member
+
+function Mathy-Edward{                      #Why doesn't it like this? WTF??? Unapproved Verb? Mathy??? Whatever PS lol
+    # Parameter help description
+    Param(
+        $Number1,
+        $Number2
+    )
+        $Number1
+        $Number2
+        Return $web.add($Number1,$Number2)
+    }
+    Mathy-Edward (1,2) -number1 1 -number2 2
